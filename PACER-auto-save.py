14c92ec63fb0025af90229362626f85a988cabe4
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-23 19:45:56 -0400

    Supposed to go through ALL emails and save the attachments

--- PACER-auto-save.py
+++ PACER-auto-save.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 from googleapiclient import discovery
 from googleapiclient import errors
 from httplib2 import Http
@@ -21,8 +20,8 @@ if not creds or creds.invalid:
 GMAIL = discovery.build('gmail', 'v1', http=creds.authorize(Http()))
 
 # this will get a list of messages, which will will return a list of messages matching the search
-message_list=GMAIL.users().messages().list(userId='me', q='from:ECFdocuments@pacerpro.com is:unread').execute()
-
+#message_list=GMAIL.users().messages().list(userId='me', q='from:ECFdocuments@pacerpro.com is:unread').execute()
+message_list=GMAIL.users().messages().list(userId='me', q='from:ECFdocuments@pacerpro.com').execute()
 if message_list['resultSizeEstimate'] > 0:
 
     for m in message_list['messages']: 
@@ -36,6 +35,19 @@ if message_list['resultSizeEstimate'] > 0:
                 message_id = m['id']
                 filename = message['snippet'].split(' ')[0] + ' _ ' + part['filename']
                 attachment=GMAIL.users().messages().attachments().get(userId='me', messageId=message_id, id=attachment_id).execute() 
+if message_list['resultSizeEstimate'] > 0:
+
+    for m in message_list['messages']: 
+
+        message = GMAIL.users().messages().get(userId='me', id=m['id'], format='full').execute()
+
+        for part in message['payload']['parts'][1:]:
+
+            if part['mimeType'] == 'application/pdf':
+                attachment_id = part['body']['attachmentId']
+                message_id = id['id']
+                filename = message['snippet'].split(' ')[0] + ' _ ' + re.search(r'(\[dckt \d+_\d*\])', filename).group(0) + " _ " + re.search(r'(.+?)\[', filename).group(1).strip()
+                attachment=GMAIL.users().messages().attachments().get(userId='me', messageId=message_id, id=attachment_id).execute() 
 
                 #print("message_id: {}; filename: {}; attachment_id: {}".format(message_id, filename, attachment_id))
             
@@ -43,8 +55,9 @@ if message_list['resultSizeEstimate'] > 0:
                 with open (PATH + filename, 'wb') as f:
                     f.write(base64.urlsafe_b64decode(attachment['data'].encode('UTF-8')))
 
-                    
+            
+                # this will write the attachment to a file
+                with open (PATH + filename, 'wb') as f:
+                    f.write(base64.urlsafe_b64decode(attachment['data'].encode('UTF-8')))
 
-
-
-    
+                print("Saved file: {}".format( PATH+ filename))
