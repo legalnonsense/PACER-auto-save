28f1e7bee5604e567ed827730a2217bcb81c472a
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-23 16:42:46 -0400

    Working to open connection, and saving attachments. Needs fixes for filenames.

--- PACER-auto-save.py
+++ PACER-auto-save.py
@@ -6,10 +6,11 @@ from googleapiclient import errors
 from httplib2 import Http
 from oauth2client import file, client, tools
 import os
+import re
 import base64
 
 
-
+PATH = "/home/jeff/Cases/Dockets/"
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 store = file.Storage(os.path.expanduser('~/.config/i3/py3status/token.json'))
 creds = store.get()
@@ -20,25 +21,29 @@ if not creds or creds.invalid:
 GMAIL = discovery.build('gmail', 'v1', http=creds.authorize(Http()))
 
 # this will get a list of messages, which will will return a list of messages matching the search
-messages=GMAIL.users().messages().list(userId='me', q='from:ECFdocuments@pacerpro.com is:unread', format='full').execute()
+message_list=GMAIL.users().messages().list(userId='me', q='from:ECFdocuments@pacerpro.com is:unread').execute()
 
-# this will get the specific message
-message=GMAIL.users().messages().get(userId='me', id='MESSAGE ID', format='full').execute()
+if message_list['resultSizeEstimate'] > 0:
 
-# this will get the attachment
-attachment=GMAIL.users().messages().attachments().get(userId='me', messageId='MESSAGE ID', id='ATTACHMENT ID').execute() 
-# question: what if there are multiple attachments?
+    for m in message_list['messages']: 
 
+        message = GMAIL.users().messages().get(userId='me', id=m['id'], format='full').execute()
 
-#figure out the file name
-#figure out the path
+        for part in message['payload']['parts'][1:]:
 
+            if part['mimeType'] == 'application/pdf':
+                attachment_id = part['body']['attachmentId']
+                message_id = m['id']
+                filename = message['snippet'].split(' ')[0] + ' _ ' + part['filename']
+                attachment=GMAIL.users().messages().attachments().get(userId='me', messageId=message_id, id=attachment_id).execute() 
 
-# this will write the attachment to a file
-with open ("PATH", 'wb') as f:
-    f.write(base64.urlsafe_b64decode(attachment['data'].encode('UTF-8')))
+                #print("message_id: {}; filename: {}; attachment_id: {}".format(message_id, filename, attachment_id))
+            
+                # this will write the attachment to a file
+                with open (PATH + filename, 'wb') as f:
+                    f.write(base64.urlsafe_b64decode(attachment['data'].encode('UTF-8')))
 
-
+                    
 
 
 
