aeae5218b78e1cb7ecf5a3ed20d343cba47b64e9
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-23 20:36:11 -0400

    Stable for getting incoming and saving PDFs with a reasonable name. Does not traverse all messages.

--- PACER-auto-save.py
+++ PACER-auto-save.py
@@ -17,11 +17,15 @@ if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets(os.path.expanduser('~/.config/i3/py3status/credentials.json'), SCOPES)
     creds = tools.run_flow(flow, store)
 
+    
 GMAIL = discovery.build('gmail', 'v1', http=creds.authorize(Http()))
 
 # this will get a list of messages, which will will return a list of messages matching the search
-#message_list=GMAIL.users().messages().list(userId='me', q='from:ECFdocuments@pacerpro.com is:unread').execute()
-message_list=GMAIL.users().messages().list(userId='me', q='from:ECFdocuments@pacerpro.com').execute()
+message_list=GMAIL.users().messages().list(userId='me', q='from:ECFdocuments@pacerpro.com is:unread').execute()
+#message_list=GMAIL.users().messages().list(userId='me', q='from:ECFdocuments@pacerpro.com').execute()
+
+
+
 if message_list['resultSizeEstimate'] > 0:
 
     for m in message_list['messages']: 
@@ -35,26 +39,7 @@ if message_list['resultSizeEstimate'] > 0:
                 message_id = m['id']
                 filename = message['snippet'].split(' ')[0] + ' _ ' + part['filename']
                 attachment=GMAIL.users().messages().attachments().get(userId='me', messageId=message_id, id=attachment_id).execute() 
-if message_list['resultSizeEstimate'] > 0:
-
-    for m in message_list['messages']: 
-
-        message = GMAIL.users().messages().get(userId='me', id=m['id'], format='full').execute()
-
-        for part in message['payload']['parts'][1:]:
-
-            if part['mimeType'] == 'application/pdf':
-                attachment_id = part['body']['attachmentId']
-                message_id = id['id']
-                filename = message['snippet'].split(' ')[0] + ' _ ' + re.search(r'(\[dckt \d+_\d*\])', filename).group(0) + " _ " + re.search(r'(.+?)\[', filename).group(1).strip()
-                attachment=GMAIL.users().messages().attachments().get(userId='me', messageId=message_id, id=attachment_id).execute() 
-
                 #print("message_id: {}; filename: {}; attachment_id: {}".format(message_id, filename, attachment_id))
-            
-                # this will write the attachment to a file
-                with open (PATH + filename, 'wb') as f:
-                    f.write(base64.urlsafe_b64decode(attachment['data'].encode('UTF-8')))
-
             
                 # this will write the attachment to a file
                 with open (PATH + filename, 'wb') as f:
